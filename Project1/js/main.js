fe28f89035c3fdc82a7e6b813470a64d3b9a1970
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2013-02-04 20:36:40 -0700

    Added meta and script to html. updated jQuery file to 1.9.1

--- Project1/js/main.js
+++ Project1/js/main.js
@@ -4,3 +4,45 @@ ASD 1302
 Project 1
 2-4-13
 */
+
+$('#home').on('pageinit', function(){
+	//code needed for home page goes here
+});	
+		
+$('#addItem').on('pageinit', function(){
+
+		var myForm = $('#formId');
+		    myForm.validate({
+			invalidHandler: function(form, validator) {
+			},
+			submitHandler: function() {
+		var data = myForm.serializeArray();
+			storeData(data);
+		}
+	});
+	
+	//any other code needed for addItem page goes here
+	
+});
+
+//The functions below can go inside or outside the pageinit function for the page in which it is needed.
+
+var autofillData = function (){
+	 
+};
+
+var getData = function(){
+
+};
+
+var storeData = function(data){
+	
+}; 
+
+var	deleteItem = function (){
+			
+};
+					
+var clearLocal = function(){
+
+};
