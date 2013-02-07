4f5df034365306140cbaa9de09d3bcb2f89c745d
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2013-02-06 22:18:24 -0700

    Added data.json and function to load it. Various tweaks to css and html

--- Project1/js/main.js
+++ Project1/js/main.js
@@ -6,7 +6,30 @@ Project 1
 */
 
 $('#index').on('pageinit', function(){
-	//code needed for home page goes here
+	//Function to load JSON dummy data from data.json
+	$("#loadJSON").on("click", function() {
+		
+		$.ajax({
+				url: "js/data.json",
+				type: "GET",
+				dataType: "json",
+				success: function(data) {
+					alert("Here's JSON!");
+						for (var i=0, j=data.dates.length; i<j; i++){
+							var myJSON = data.dates[i];
+								$("<div>" + "<ul id='myDisp' data-role='listview'>" + 
+									"<li>" + myJSON.events + "</li>" + 
+									"<li>" + myJSON.evdate + "</li>" + 
+									"<li>" + myJSON.evinfo + "</li>" + 
+									"<li>" + myJSON.importance + "</li>" + 
+									"<li>" + myJSON.attend + "</li>" + 
+									"<li>" + myJSON.details + "</li>" + 
+									"</ul>" + "</div>").appendTo("#dispData");
+				}
+			}
+		})
+	});
+	
 });	
 		
 $('#addItem').on('pageinit', function(){
@@ -28,7 +51,12 @@ $('#addItem').on('pageinit', function(){
 //The functions below can go inside or outside the pageinit function for the page in which it is needed.
 
 var autofillData = function (){
-	 
+	//The actual JSON object data required for this is coming from json.js, which is loaded from our HTML page
+	//Store the JSON object into Local Storage
+	    for(var n in json) {
+		    var id = Math.floor(Math.random()*100000001);
+		    localStorage.setItem(id, JSON.stringify(json[n]));	    
+		} 
 };
 
 var getData = function(){
