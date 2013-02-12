cf85979658f314479cd5fb6299b7b4fa92b9fbb4
Elijah Freestone <elojahfreestone@Elijahs-MacBook-Pro.local>
2013-02-11 21:55:06 -0700

    Working on saveData function. Saves to LS but issue with key coming up as Obj instead of #s

--- Project2/js/main.js
+++ Project2/js/main.js
@@ -37,19 +37,62 @@ $('#index').on('pageinit', function(){
 		
 $('#addItem').on('pageinit', function(){
 
-		var myForm = $('#formId');
-		    myForm.validate({
-			invalidHandler: function(form, validator) {
+	var myForm = $("#eventForm"),
+		popErrors = $('#popErrors');
+			
+		myForm.validate({
+		invalidHandler: function(form, validator) {
+			popErrors.click(); //Pop up error dialog box
+			var html = '';
+				for(var key in validator.submitted) {
+					var label = $('label[for^="'+ key +'"]') // Selector search for required labels not filled out
+					var legend = label.closest('fieldset').find('ui-controlgroup-label');
+					var fieldName = legend.length ? legend.text() : label.text();
+					html += '<li>'+ fieldName +'</li>';
+				};
+				$("#formErrors ul").html(html); // Fill out Pop up error text
 			},
 			submitHandler: function() {
 		var data = myForm.serializeArray();
-			storeData(data);
+			saveData(data);
+			//console.log(key);
 		}
 	});
 	
-	//any other code needed for addItem page goes here
+	$("submit").on("click", function() {
+		saveData();
+	});
+
+//Function to add key and save data to local storage
+	function saveData(key) {
+   		//If there is no key, this means this is a brand new item and we need a new key.
+	   	if(!key) {
+			var id = Math.floor(Math.random()*100000001);
+		}else{
+			//Set the id to the existing key that we're editing so it will save over the data.
+			//The key is the same key that has been passed along form the editSubmit handler
+			//to the validate function, and then passed here, into the storeData finction.
+			id = key;
+		}
+        //Gather up all our form field values and store in an object.
+        //Object properties contain array with the form label and input value.
+    //getSelectedRadio();
+        var item         = {};
+            item.events   = ["Event:", $("#eventType").val()]; //Event type selector
+            item.evdate  = ["Date:", $("#evDate").val()]; //Event Date
+            item.evinfo  = ["Info:", $("#evInfo").val()]; //Event Info
+            item.importance = ["Importance:", $("#importance").val()]; //Event Importance Slider
+            //item.attend = ["Is attendance required?:", attendValue]; //Attendance Radio Buttons
+            item.details = ["Event Details:", $("#details").val()]; //Event Details
+            
+        //Save Data into Local Storage: Use Stringify to convert object to a string.
+        localStorage.setItem(id, JSON.stringify(item));
+        alert("Date Saved!");
+        //window.location.reload();
+    };
 	
-});
+		
+}); //End of addItem pageinit
 
 //The functions below can go inside or outside the pageinit function for the page in which it is needed.
 
@@ -113,10 +156,12 @@ $("#clearData").on("click", function() {
 	if(localStorage.length === 0) {
     	alert("No data to clear.");
     }else{
-    var ask = confirm("Are you sure you want to delete all events?");
-            localStorage.clear();
-            alert("All events have been removed!");
-            window.location.reload();
-            return false;     
+    	var ask = confirm("Are you sure you want to delete all events?");
+    	localStorage.clear();
+    	alert("All events have been removed!");
+        window.location.reload();
+        	return false;     
     }
 });
+
+
